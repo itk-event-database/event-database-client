ce84df0aee6e331f77aa93d61197d64dc30e9c8a
Mikkel Ricky <rimi@aarhus.dk>
2016-09-14 16:00:49 +0200

    EV-51: Added Place to Occurrence

--- src/Item/Occurrence.php
+++ src/Item/Occurrence.php
@@ -3,6 +3,16 @@
 namespace Itk\EventDatabaseClient\Item;
 
 class Occurrence extends Item {
+  protected $place;
+
+  public function __construct(array $data) {
+    parent::__construct($data);
+
+    if ($this->get('place')) {
+      $this->place = new Place($this->get('place'));
+    }
+  }
+
   public function __toString() {
     $s = $this->getStartDate();
     $s .= '–';
@@ -18,8 +28,8 @@ class Occurrence extends Item {
     return $this->get('endDate');
   }
 
-  public function getVenue() {
-    return $this->get('venue');
+  public function getPlace() {
+    return $this->place;
   }
 
 }
---
+++ src/Item/Place.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Itk\EventDatabaseClient\Item;
+
+class Place extends Item {
+  public function __toString() {
+    return $this->getName() ?: '';
+  }
+
+  public function getName() {
+    return $this->get('name');
+  }
+
+}
